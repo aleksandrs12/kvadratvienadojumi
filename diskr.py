8669096c700239a224fc2a3d9249632c0972d8f5
aleksandrs12 <90208363+aleksandrs12@users.noreply.github.com>
2021-10-04 16:23:37 +0300

    Update diskr.py

--- diskr.py
+++ diskr.py
@@ -5,8 +5,6 @@ while True:
     a = float( input( "ievadiet a" ) )
     b = float( input( "ievadiet b" ) )
     c = float( input( "ievadiet c" ) )
-    ddd = b ** 2
-    dd = 4 * a * c
     d = b ** 2 - 4 * a * c
     if d > 0:
         x1 = (d ** 0.5 + b * -1) / (2 * a)
